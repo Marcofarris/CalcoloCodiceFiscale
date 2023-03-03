ba265dbe0a0938e2e1a49d4d84d8cbef077545b2
marco.farris <marco.farris@WINAPJQBJEFHUND>
2023-03-03 12:23:55 +0100

    Add project files.

---
+++ CalcoloCodiceFiscale/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace CalcoloCodiceFiscale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.comboComuni = new System.Windows.Forms.ComboBox();
+            this.Comune = new System.Windows.Forms.Label();
+            this.radioM = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcola = new System.Windows.Forms.Button();
+            this.txtCodice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(134, 64);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(162, 26);
+            this.txtNome.TabIndex = 0;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(35, 64);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(51, 20);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cognome";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(134, 134);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(151, 26);
+            this.txtCognome.TabIndex = 3;
+            // 
+            // comboComuni
+            // 
+            this.comboComuni.FormattingEnabled = true;
+            this.comboComuni.Location = new System.Drawing.Point(134, 226);
+            this.comboComuni.Name = "comboComuni";
+            this.comboComuni.Size = new System.Drawing.Size(121, 28);
+            this.comboComuni.TabIndex = 4;
+            // 
+            // Comune
+            // 
+            this.Comune.AutoSize = true;
+            this.Comune.Location = new System.Drawing.Point(39, 226);
+            this.Comune.Name = "Comune";
+            this.Comune.Size = new System.Drawing.Size(69, 20);
+            this.Comune.TabIndex = 5;
+            this.Comune.Text = "Comune";
+            // 
+            // radioM
+            // 
+            this.radioM.AutoSize = true;
+            this.radioM.Location = new System.Drawing.Point(65, 34);
+            this.radioM.Name = "radioM";
+            this.radioM.Size = new System.Drawing.Size(47, 24);
+            this.radioM.TabIndex = 7;
+            this.radioM.TabStop = true;
+            this.radioM.Text = "M";
+            this.radioM.UseVisualStyleBackColor = true;
+            // 
+            // radioF
+            // 
+            this.radioF.AutoSize = true;
+            this.radioF.Location = new System.Drawing.Point(6, 34);
+            this.radioF.Name = "radioF";
+            this.radioF.Size = new System.Drawing.Size(44, 24);
+            this.radioF.TabIndex = 8;
+            this.radioF.TabStop = true;
+            this.radioF.Text = "F";
+            this.radioF.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioF);
+            this.groupBox1.Controls.Add(this.radioM);
+            this.groupBox1.Location = new System.Drawing.Point(39, 303);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sesso";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(471, 114);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 26);
+            this.datePicker.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(467, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Data di nascita";
+            // 
+            // btnCalcola
+            // 
+            this.btnCalcola.Location = new System.Drawing.Point(499, 226);
+            this.btnCalcola.Name = "btnCalcola";
+            this.btnCalcola.Size = new System.Drawing.Size(103, 28);
+            this.btnCalcola.TabIndex = 12;
+            this.btnCalcola.Text = "Calcola";
+            this.btnCalcola.UseVisualStyleBackColor = true;
+            this.btnCalcola.Click += new System.EventHandler(this.btnCalcola_Click);
+            // 
+            // txtCodice
+            // 
+            this.txtCodice.Location = new System.Drawing.Point(405, 353);
+            this.txtCodice.Name = "txtCodice";
+            this.txtCodice.Size = new System.Drawing.Size(333, 26);
+            this.txtCodice.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(495, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Codice fiscale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodice);
+            this.Controls.Add(this.btnCalcola);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Comune);
+            this.Controls.Add(this.comboComuni);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.ComboBox comboComuni;
+        private System.Windows.Forms.Label Comune;
+        private System.Windows.Forms.RadioButton radioM;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcola;
+        private System.Windows.Forms.TextBox txtCodice;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalcoloCodiceFiscale/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcoloCodiceFiscale
+{
+    public partial class Form1 : Form
+    {
+        public string codice = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CalcolaCognome()
+        {
+            foreach (var c in txtCognome.Text)
+            {
+                if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && codice.Length < 3)
+                {
+                    codice += c;
+                }
+            }
+            for (int i = 0; codice.Length < 3; i++)
+            {
+                if (txtCognome.Text[i] == 'a' || txtCognome.Text[i] == 'e' || txtCognome.Text[i] == 'i' || txtCognome.Text[i] == 'o' || txtCognome.Text[i] == 'u')
+                {
+                    codice += txtCognome.Text[i];
+                }
+                else codice += "x";
+            }
+        }
+
+        public void CalcolaNome()
+        {
+            foreach (var c in txtNome.Text)
+            {
+                if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && codice.Length < 3)
+                {
+                    codice += c;
+                }
+            }
+            for (int i = 0; codice.Length < 3; i++)
+            {
+                if (txtNome.Text[i] == 'a' || txtNome.Text[i] == 'e' || txtNome.Text[i] == 'i' || txtNome.Text[i] == 'o' || txtNome.Text[i] == 'u')
+                {
+                    codice += txtNome.Text[i];
+                }
+                else codice += "x";
+            }
+        }
+
+        public void CalcolaData()
+        {
+            //Calcolo anno
+            codice += datePicker.Value.Year.ToString().Substring(datePicker.Value.Year.ToString().Length - 2);
+            codice += datePicker.Value.Year.ToString().Substring(datePicker.Value.Year.ToString().Length - 1);
+
+            //Calcolo mese  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" datePicker.Value.Month.ToString();
+            var alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var n = datePicker.Value.Month;
+            txtCodice.Text = alf[ - 1]; 
+
+            
+        }
+        private void btnCalcola_Click(object sender, EventArgs e)
+        {
+            CalcolaCognome();
+            CalcolaNome();
+            CalcolaData();
+            //txtCodice.Text = codice;
+        }
+    }
+}
